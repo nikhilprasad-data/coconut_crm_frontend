f45ba47dff566308e46a5ee77653cd72297a351c
Nikhil Prasad <contact.nikhilprasad@gmail.com>
2026-04-20 20:56:21 +0530

    feat(seller): add AddSeller page wrapper with strict JWT RBAC

---
+++ src/app/add-seller/page.js
@@ -0,0 +1,10 @@
+"use client";
+import AddSellerForm from "@/components/AddSellerForm";
+
+export default function AddSeller() {
+     return(
+          <div>
+               <AddSellerForm/>
+          </div>
+     );
+}
